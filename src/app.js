d935578e7f82aca787541f53db7751655ebe8adf
Adisudirta <yanadisudirta@gmail.com>
2022-01-03 06:41:04 -0800

    70% progress solved

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import "regenerator-runtime";
 
+// CSS
+import "./style.css";
+
 // Components
 import "./components/header-app/index.js";
+import "./components/banner-app/index.js";
+import "./components/card-form/index.js";
+import "./section/about-us/index.js";
--- src/components/header-app/index.js
+++ src/components/header-app/index.js
@@ -5,12 +5,34 @@ import css from "./style.css";
 const template = componentTemplateFactory(html, css);
 
 class HeaderApp extends HTMLElement{
-    
     connectedCallback(){
-        this.attachShadow({mode: "open"});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-    }
+        this.appendChild(template.content.cloneNode(true));
+        
+        const toggleBtn = document.getElementById('toggleBtn');
+        const mobileNav = document.querySelector('#mobile.container-menu');
+        const header = document.querySelector("header");
+        const btn = document.querySelector("#desktop.container-menu .container-button button");
+        const btn1 = document.querySelector("#desktop.container-menu .container-button button:last-child");
 
+        toggleBtn.addEventListener('click', function(){
+            mobileNav.classList.toggle('hide');
+            this.childNodes[1].classList.toggle('rotate');
+        });
+
+        window.addEventListener("scroll", function () {
+            if (this.scrollY > 10) {
+              header.classList.add("background");
+              btn.style.color = "#828282";
+              btn1.style.color = "#828282";
+              btn1.style.border = "1px solid #828282";
+            } else {
+              header.classList.remove("background");
+              btn.style.color = "white";
+              btn1.style.color = "white";
+              btn1.style.border = "1px solid white";
+            }
+        });
+    }
 }
 
 customElements.define("header-app", HeaderApp);
